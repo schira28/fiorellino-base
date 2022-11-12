aad38d90a4150e0eaa0d9872c4930935ea588ece
schira28 <schiralli.samuele@panettipitagora.edu.it>
2022-11-12 09:01:21 +0100

    finito

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(600, 300);
+}
+
+function draw() {
+  background("rgb(0,255,0)");
+  fill(255,204,0)
+  circle(68,90,130)
+  circle(68,220,130)
+  circle(532,220,130)
+  circle(532,90,130)
+  circle(360,90,130)
+  circle(360,220,130)
+  circle(230,220,130)
+  circle(230,90,130)
+  strokeWeight(4)
+  fill("red")
+  circle(295,150,130)
+  circle(0,150,130)
+  circle(598,150,130)
+}
